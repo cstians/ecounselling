c761bd188cbb9c908acaaa80295a8b4faa7e9ac9
khushwanth <khushwanth.chhetri@gmail.com>
2018-11-02 00:00:27 +0600

    Linked Depression Test button

--- app/src/main/java/bt/edu/cst/www/ecounselling/DepressionTest.java
+++ app/src/main/java/bt/edu/cst/www/ecounselling/DepressionTest.java
@@ -1,13 +1,21 @@
 package bt.edu.cst.www.ecounselling;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DepressionTest extends AppCompatActivity {
-
+    Button beginTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_depression_test);
+        beginTest=findViewById(R.id.buttonBeginTest);
+        beginTest.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(),DepressionAssesment.class));            }
+        });
     }
 }
