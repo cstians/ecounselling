3a5a5fec0d472a656fd4705d277f377b860260a5
khushwanth <khushwanth.chhetri@gmail.com>
2018-11-03 01:07:39 +0600

    Working on the Quiz Class

--- app/src/main/java/bt/edu/cst/www/ecounselling/DepressionAssesment.java
+++ app/src/main/java/bt/edu/cst/www/ecounselling/DepressionAssesment.java
@@ -1,13 +1,21 @@
 package bt.edu.cst.www.ecounselling;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DepressionAssesment extends AppCompatActivity {
-
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_depression_assesment);
+        start=findViewById(R.id.button);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(),Quiz.class));            }
+        });
     }
 }
